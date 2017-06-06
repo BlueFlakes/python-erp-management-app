f10354ebbd7d8cabf36e74e76fff58fddfe9dca1
JakubJaniszewski <cwanykuba@gmail.com>
2017-06-06 14:31:02 +0200

    Made add() function

--- hr/hr.py
+++ hr/hr.py
@@ -22,19 +22,19 @@ def choose_option(table):
     if option == "1":
         show_table(table)
     elif option == "2":
-        add(table)
+        table = add(table)
     elif option == "3":
         id_ = ui.get_inputs(["Id"], "Please provide record you want to remove")[0]
-        remove(table, id_)
+        table = remove(table, id_)
     elif option == "4":
         id_ = ui.get_inputs(["Id"], "Please provide record you want to update")[0]
-        update(table, id_)
+        table = update(table, id_)
     elif option == "5":
         get_oldest_person(table)
     elif option == "6":
         get_persons_closest_to_average(table)
 
-    return option
+    return option, table
 
 
 def start_module():
@@ -55,7 +55,9 @@ def start_module():
     option = float("inf")
     while not option == "0":
         ui.print_menu("Human resources manager", options_list, "Exit to menu")
-        option = choose_option(table)
+        option, table = choose_option(table)
+
+    data_manager.write_table_to_file("hr/persons.csv", table)
 
 
 def show_table(table):
@@ -84,7 +86,12 @@ def add(table):
         Table with a new record
     """
 
-    name, year = ui.get_inputs(["Name, Birth year"], "Please provide information")
+    id_ = common.generate_random(table)
+    data_to_add = ui.get_inputs(["Name", "Birth year"], "Please provide information")
+
+    data_to_add.insert(0, id_)
+
+    table.append(data_to_add)
 
     return table
 
